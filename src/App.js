61b6ca957f87ec2aab472b28d1b8af522322adeb
mbone1 <m.bonnev10@gmail.com>
2020-10-07 15:54:07 -0700

    maybe it'll work this time

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 // import "./App.css";
 import "./style.css";
 import Draggable, { DraggableCore } from "react-draggable";
-import Contact from "./components/Contact";
-import About from "./components/About";
-import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact.js";
+import About from "./components/About.js";
+import Portfolio from "./components/Portfolio.js";
 import LinkedInButton from "./components/LinkedInButton";
 import GithubButton from "./components/GithubButton";
 
