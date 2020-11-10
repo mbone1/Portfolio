741544f5f483e527a80528640ecf2457100b4161
mbone1 <m.bonnev10@gmail.com>
2020-11-10 13:27:10 -0700

    updated portfolio page

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -19,23 +19,25 @@ const projects = [
     name: "Progressive Budget",
     url: "https://github.com/mbone1/Online-Offline-Budget-Tracker",
     deployedUrl: "https://gentle-falls-82429.herokuapp.com/",
-    description: "Straightforward and slim budget app, allows users to enter deposits and withdrawals with a running total. A fully progressive app that works both on and offline.",
+    description:
+      "Straightforward and slim budget app, allows users to enter deposits and withdrawals with a running total. A fully progressive app that works both on and offline.",
     screenshot: "https://i.ibb.co/6Wv0HKG/onlineoffline.jpg",
   },
-  {
-    name: "Fitness Tracker",
-    url: "https://github.com/mbone1/Workout-Tracker",
-    deployedUrl:
-      "https://salty-peak-19644.herokuapp.com/?id=5f6ea4e17d3b0d00170b0682",
-    description: "",
-    screenshot: "",
-  },
+  //broken
+  // {
+  //   name: "Fitness Tracker",
+  //   url: "https://github.com/mbone1/Workout-Tracker",
+  //   deployedUrl:
+  //     "https://salty-peak-19644.herokuapp.com/?id=5f6ea4e17d3b0d00170b0682",
+  //   description: "",
+  //   screenshot: "",
+  // },
   {
     name: "Albuy Marketplace",
     url: "https://github.com/mbone1/Albuy-Marketplace",
     deployedUrl: "https://fathomless-eyrie-52685.herokuapp.com/",
-    description: "",
-    screenshot: "",
+    description: "An marketplace-lite app for buying and selling vinyl albums. Uses Spotify's API for search function. Technologies used include: Node, Express, Handlebars.js, MySQL w/ Sequelize.",
+    screenshot: "https://i.ibb.co/pzwwG5p/albuymarketplace.jpg",
   },
   {
     name: "Eatdaburrito",
