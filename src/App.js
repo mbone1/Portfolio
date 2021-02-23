851fea08abd61efa32f2f97cdd5fd753abcfb6a6
mbone1 <m.bonnev10@gmail.com>
2021-02-23 16:25:51 -0700

    converted fully to hooks

--- src/App.js
+++ src/App.js
@@ -1,107 +1,71 @@
-import React, { Component } from "react";
-// import "./App.css";
-import "./style.css";
-import Draggable, { DraggableCore } from "react-draggable";
-import Contact from "./components/Contact.js";
+import { useState } from "react";
 import About from "./components/About.js";
+import Contact from "./components/Contact.js";
 import Portfolio from "./components/Portfolio.js";
 import LinkedInButton from "./components/LinkedInButton";
 import GithubButton from "./components/GithubButton";
-// import { Link } from "react-router-dom";
+import Draggable, { DraggableCore } from "react-draggable";
 
+export default function App() {
+  const [display, setDisplay] = useState("none");
+  //will handle state of side panel, about/contact/portfolio
+  return (
+    <div className="App">
+      <section className="container">
+        <div className="columns is-multiline is-flex-mobile">
+          <div className="column is-8 is-offset-2 register">
+            <div className="columns">
+              <div className="column left">
+                <DraggableCore>
+                  <h2 className="title is-1">Matt Bonneville</h2>
+                </DraggableCore>
+                <Draggable>
+                  <button
+                    className="button is-block is-outlined is-dark is-fullwidth is-light"
+                    id="about"
+                    onClick={() => {
+                      setDisplay("about");
+                    }}>
+                    about
+                  </button>
+                </Draggable>
+                <Draggable>
+                  <button
+                    className="button is-block is-outlined is-dark is-fullwidth is-light"
+                    id="portfolio"
+                    onClick={() => {
+                      setDisplay("portfolio");
+                    }}>
+                    portfolio
+                  </button>
+                </Draggable>
+                <Draggable>
+                  <button
+                    className="button is-block is-outlined is-dark is-fullwidth is-light"
+                    id="contact"
+                    onClick={() => {
+                      setDisplay("contact");
+                    }}>
+                    contact
+                  </button>
+                </Draggable>
+                <GithubButton /> <LinkedInButton />{" "}
+              </div>
 
-
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      showAbout: false,
-      showContact: false,
-      showPortfolio: false,
-    };
-  }
-
-  
-  render() {
-    return (
-      <div className="App">
-        <section className="container">
-          <div className="columns is-multiline is-flex-mobile">
-            <div className="column is-8 is-offset-2 register">
-              <div className="columns">
-                <div className="column left">
-                  <DraggableCore>
-                    <h2 className="title is-1">Matt Bonneville</h2>
-                  </DraggableCore>
-                  <Draggable>
-                    <button
-                      className="button is-block is-outlined is-dark is-fullwidth is-light"
-                      id="about"
-                      onClick={() => {
-                        this.setState({
-                          showAbout: !this.state.showAbout,
-                          showContact: false,
-                          showPortfolio: false,
-                        });
-                      }}>
-                      {this.state.show ? "about" : "about"}
-                    </button>
-                  </Draggable>
-                  <Draggable>
-                    <button
-                      className="button is-block is-outlined is-dark is-fullwidth is-light"
-                      id="portfolio"
-                      onClick={() => {
-                        this.setState({
-                          showPortfolio: !this.state.showPortfolio,
-                          showContact: false,
-                          showAbout: false,
-                        });
-                      }}>
-                      {this.state.show ? "portfolio" : "portfolio"}
-                    </button>
-                  </Draggable>
-                  <Draggable>
-                    <button
-                      className="button is-block is-outlined is-dark is-fullwidth is-light"
-                      id="contact"
-                      onClick={() => {
-                        this.setState({
-                          showContact: !this.state.showContact,
-                          showAbout: false,
-                          showPortfolio: false,
-                        });
-                      }}>
-                      {this.state.showContact ? "contact" : "contact"}
-                    </button>
-                  </Draggable>
-                  <GithubButton />
-                  
-                    {" "}
-                    <LinkedInButton />{" "}
-                
-                </div>
-                <div className="column right has-text-centered scroll scroller" id="rightPanel scroller">
-                  {/* <!-- beginning of about page --> */}
-                  {this.state.showAbout ? <About /> : null}
-                  {/* <!-- end of about page -->
-                        <!-- beginning of contact page --> */}
-                  {this.state.showContact ? <Contact /> : null}
-                  {/* <!-- end of contact page --> */}
-                  {/* <!-- beginning of portfolio page --> */}
-                  {this.state.showPortfolio ? <Portfolio /> : null}
-                  {/* <!-- end of portfolio page --> */}
-                </div>
+              <div
+                className="column right has-text-centered scroll scroller"
+                id="rightPanel scroller">
+                {display === "about" ? <About /> : null}
+                {display === "contact" ? <Contact /> : null}
+                {display === "portfolio" ? <Portfolio /> : null}
               </div>
             </div>
           </div>
-        </section>
-      </div>
-    );
-  }
+        </div>
+      </section>
+    </div>
+  );
 }
-export default App;
 
        
              
--- src/App2.js
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-
-export default function App() {
-    const [display, setDisplay] = useState("none")
-    //will handle state of side panel
-
-}
