4510c69227788470c68350d4071d1a5923eda4c1
mbone1 <m.bonnev10@gmail.com>
2021-03-13 17:26:49 -0700

    added 3d face model

--- src/App2.js
+++ src/App2.js
@@ -4,9 +4,10 @@ import Screencap from './assets/elyucateco.jpg'
 // import ThreeScene from './components/cube2'
 import { Canvas, useLoader, useFrame } from "react-three-fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import Laptop from './laptopweather.glb'
-import Environment from './components/environment'
+import Laptop from './facefinal.glb'
 import Lights from './components/lights'
+import { useSpring, animated } from 'react-spring/three'
+import * as THREE from "three";
 
 
 
@@ -16,17 +17,26 @@ import Lights from './components/lights'
 export default function App2() {
 
   const [view, setView] = useState()
-  const [project, setProject] =useState(Laptop)
+  const [project, setProject] = useState(Laptop)
+  const [active, setActive] = useState()
+  const { pos, ...props } = useSpring({
+    pos: active ? [0, -2, 2] : [0, -2, 2],
+    scale: active ? [3, 3, 3] : [4, 4, 4],
+    rotation: active
+      // ? [THREE.Math.degToRad(1080), 0, THREE.Math.degToRad(45)]
+      ? [0, -.1, 0]
+      : [0, -.5, THREE.Math.degToRad(1080)],
+    config: { mass: 100, tension: 1000, friction: 300, precision: 0.00001 },
+  });
 
   // function
   function Spinny() {
-    const group = useRef()
+    // const group = useRef()
     const gltf = useLoader(GLTFLoader, project)
-    useFrame(() => group.current.rotation.y += .02);
     return (
-      <group ref={group}>
+      <group>
       {/* <group rotation={[0, 0, 0]}> */}
-        <primitive object={gltf.scene} position={[0, -3, 0]} scale={[5,5,5]}/>
+        <animated.primitive onClick={e => setActive(!active)} object={gltf.scene}{...props}/>
       {/* </group> */}
       </group>
     );
@@ -57,13 +67,23 @@ export default function App2() {
               <button onClick={() => setView("about")}></button>
               {view === "about" ? (
                 <div>
+                  
                   <Canvas camera={{ position: [0, 0, 10]}}>
                     {/* <Environment /> */}
                    <Suspense fallback={<Box/>}>{<Spinny />}</Suspense>
                   <Lights/>
-                  </Canvas>
-                  <div>Spingus tingus tongus, spong the crongus!</div>
-                </div>
+                    </Canvas>
+                   
+                  <div className="i">
+                  <h4>Technologies</h4>
+                  <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima sint vero minus, nam eum ex temporibus ea sequi aspernatur, molestiae reiciendis dignissimos. Voluptatem facere sunt nam beatae dignissimos laborum voluptate.</span>
+                  <h4>Description</h4>
+                  <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo sunt voluptatum corrupti reiciendis deserunt? Natus sequi, nemo ipsa aspernatur officia ea veritatis perspiciatis dolorem blanditiis eos tempore facere. Vero, rerum?</span>
+                  <h4>Links</h4>
+                  <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo sunt voluptatum corrupti reiciendis deserunt? Natus sequi, nemo ipsa aspernatur officia ea veritatis perspiciatis dolorem blanditiis eos tempore facere. Vero, rerum?</span>
+                
+                  </div>
+                  </div>
               ) : (
                 <></>
               )}
