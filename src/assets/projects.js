9d93b68194a3bfbdc854655e70c83945d8861569
mbone1 <m.bonnev10@gmail.com>
2021-04-05 14:57:53 -0700

    added caravaggio project, 3d laptop model + entry in projects

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -3,68 +3,79 @@ import burrito from './laptopburrito.glb'
 import lplan from './laptoplpan.glb'
 import employee from './laptoponlineemployee.glb'
 import budget from './laptoponlineoffline.glb'
+import caravaggio from './laptopcaravaggio.glb'
 
 const projects = [
   {
     key: "1",
+    name: "Caravaggio",
+    url: "https://github.com/mbone1/caravaggio",
+    deployedUrl: "https://caravaggio.netlify.app/",
+    technologies:
+      "React.js, Three.js, React-Three-Fiber, Three-Drei, React-Spring",
+    description:
+      "Virtual art gallery of Caravaggio paintings, built on React in Three.js with help from Fiber and Drei, animated with React-Spring",
+    model: caravaggio,
+  },
+  {
+    key: "2",
     name: "L-Plan",
     url: "https://github.com/scottdouglas238/Lesson_Planner",
     deployedUrl: "https://sheltered-hollows-22179.herokuapp.com/",
     technologies: "React.js, Express, Node.js, MongoDB, Bulma, Sass",
     description: "MERN Stack, CRUD application for lesson plans",
-    model: lplan
-  },
-  {
-    key: "2",
-    name: "React-Employee-Database",
-    url: "https://github.com/mbone1/React-Employee-Database",
-    deployedUrl: "https://react-employee-db.netlify.app/",
-    description:
-      "Employee Database with various filtering capabilities",
-    technologies: "React.js, JS, CSS",
-    model: employee
+    model: lplan,
   },
   {
     key: "3",
-    name: "Progressive Budget",
-    url: "https://github.com/mbone1/Online-Offline-Budget-Tracker",
-    deployedUrl: "https://gentle-falls-82429.herokuapp.com/",
-    description:
-      "PWA Budget Application",
-    technologies: "Express, Mongoose, JS",
-    model: budget
+    name: "React-Employee-Database",
+    url: "https://github.com/mbone1/React-Employee-Database",
+    deployedUrl: "https://react-employee-db.netlify.app/",
+    description: "Employee Database with various filtering capabilities",
+    technologies: "React.js, JS, CSS",
+    model: employee,
   },
   {
     key: "4",
+    name: "Progressive Budget",
+    url: "https://github.com/mbone1/Online-Offline-Budget-Tracker",
+    deployedUrl: "https://gentle-falls-82429.herokuapp.com/",
+    description: "PWA Budget Application",
+    technologies: "Express, Mongoose, JS",
+    model: budget,
+  },
+  {
+    key: "5",
     name: "Albuy Marketplace",
     url: "https://github.com/mbone1/Albuy-Marketplace",
     deployedUrl: "https://fathomless-eyrie-52685.herokuapp.com/",
     description:
-    "An album marketplace using spotifys API for search functionality",
-    technologies:  "Node, Express, Handlebars.js, MySQL w/ Sequelize",
-    model: albuy
+      "An album marketplace using spotifys API for search functionality",
+    technologies: "Node, Express, Handlebars.js, MySQL w/ Sequelize",
+    model: albuy,
   },
   {
-      key: "5",
-      name: "Eatdaburrito",
-      url: "https://github.com/mbone1/eatdaburrito",
-      deployedUrl: "https://safe-garden-94512.herokuapp.com/",
-      description: "Looking for a way to keep track of the burritos you've eaten? Look no further!",
-      technologies: "Handlebars.js, Express, MySQL",
-      model: burrito
-    }
-    //broken
-    // {
-      //   name: "Fitness Tracker",
-      //   url: "https://github.com/mbone1/Workout-Tracker",
-      //   deployedUrl:
-    //     "https://salty-peak-19644.herokuapp.com/?id=5f6ea4e17d3b0d00170b0682",
-    //   description: "",
-    //   screenshot: "",
-    // },
+    key: "6",
+    name: "Eatdaburrito",
+    url: "https://github.com/mbone1/eatdaburrito",
+    deployedUrl: "https://safe-garden-94512.herokuapp.com/",
+    description:
+      "Looking for a way to keep track of the burritos you've eaten? Look no further!",
+    technologies: "Handlebars.js, Express, MySQL",
+    model: burrito,
+  },
+  //broken
+  // {
+  //   name: "Fitness Tracker",
+  //   url: "https://github.com/mbone1/Workout-Tracker",
+  //   deployedUrl:
+  //     "https://salty-peak-19644.herokuapp.com/?id=5f6ea4e17d3b0d00170b0682",
+  //   description: "",
+  //   screenshot: "",
+  // },
   //not a deployable app
   // {
-    //   name: "Employee Organizer",
+  //   name: "Employee Organizer",
   //   url: "https://github.com/mbone1/employee-organizer",
   //   deployedUrl: null,
   //   description: "",
